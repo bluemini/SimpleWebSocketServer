ef754a0342e093dcc2872ba9e8d60cb21cdd64a7
Nick Harvey <nick@bluemini.com>
2013-01-02 15:00:29 -0800

    some comments on main

--- src/com/bluemini/websockets/SimpleServer.java
+++ src/com/bluemini/websockets/SimpleServer.java
@@ -37,8 +37,14 @@ public class SimpleServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// We're going to use the echo handler to simply echo back the message
 		Server s = new Server(new EchoHandler());
+		
+		// we'll accept connections from all domains
 		s.setHost("*");
+		
+		// start the WebSocket server
 		new Thread(s, "WebSocketServer").start();
 	}
 
