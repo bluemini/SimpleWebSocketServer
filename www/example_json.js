aa59ce40fa0bd7286564ddea9db5a2c84e1b235e
Nick Harvey <nick@bluemini.com>
2013-01-02 15:05:42 -0800

    first go at the web part of WebRTC
    looking at implementing WebRTC over the simpleWebSocketServer. The JSON
    message passing is working as is the unmasking of messages and multi
    frame messages :)
    
    Need to do more work on the JS parts.

---
+++ www/example_json.js
@@ -0,0 +1,48 @@
+{
+	"sdp": {
+		"sdp":
+			"v=0\r\n
+			o=- 1212143194 2 IN IP4 127.0.0.1\r\n
+			s=-\r\n
+			t=0 0\r\n
+			a=group:BUNDLE audio video\r\n
+			m=audio 1 RTP/SAVPF 103 104 0 8 106 105 13 126\r\n
+			c=IN IP4 0.0.0.0\r\n
+			a=rtcp:1 IN IP4 0.0.0.0\r\n
+			a=ice-ufrag:nT5G50/9Z6oWfHXD\r\n
+			a=ice-pwd:90CEZSxjmO1VipjRx5wb5yR5\r\n
+			a=ice-options:google-ice\r\n
+			a=sendrecv\r\n
+			a=mid:audio\r\n
+			a=rtcp-mux\r\n
+			a=crypto:1 AES_CM_128_HMAC_SHA1_80 inline:KfTFJnwun4WjQBbGgs9C3tbmDcN1TQPbkTqAbueH\r\n
+			a=rtpmap:103 ISAC/16000\r\n
+			a=rtpmap:104 ISAC/32000\r\n
+			a=rtpmap:0 PCMU/8000\r\n
+			a=rtpmap:8 PCMA/8000\r\n
+			a=rtpmap:106 CN/32000\r\n
+			a=rtpmap:105 CN/16000\r\n
+			a=rtpmap:13 CN/8000\r\n
+			a=rtpmap:126 telephone-event/8000\r\n
+			a=ssrc:2891061256 cname:BYWYfUK1YXt0MMCJ\r\n
+			a=ssrc:2891061256 mslabel:8LWI9KrPSoh6Af5jCc4zZ0rnHEAZLPl1Jr1C\r\n
+			a=ssrc:2891061256 label:8LWI9KrPSoh6Af5jCc4zZ0rnHEAZLPl1Jr1C00\r\n
+			m=video 1 RTP/SAVPF 100 101 102\r\n
+			c=IN IP4 0.0.0.0\r\n
+			a=rtcp:1 IN IP4 0.0.0.0\r\n
+			a=ice-ufrag:nT5G50/9Z6oWfHXD\r\n
+			a=ice-pwd:90CEZSxjmO1VipjRx5wb5yR5\r\n
+			a=ice-options:google-ice\r\n
+			a=sendrecv\r\n
+			a=mid:video\r\n
+			a=rtcp-mux\r\n
+			a=crypto:1 AES_CM_128_HMAC_SHA1_80 inline:KfTFJnwun4WjQBbGgs9C3tbmDcN1TQPbkTqAbueH\r\n
+			a=rtpmap:100 VP8/90000\r\n
+			a=rtpmap:101 red/90000\r\n
+			a=rtpmap:102 ulpfec/90000\r\n
+			a=ssrc:2328107083 cname:BYWYfUK1YXt0MMCJ\r\n
+			a=ssrc:2328107083 mslabel:8LWI9KrPSoh6Af5jCc4zZ0rnHEAZLPl1Jr1C\r\n
+			a=ssrc:2328107083 label:8LWI9KrPSoh6Af5jCc4zZ0rnHEAZLPl1Jr1C10\r\n",
+		"type":"offer"
+	}
+}
